b2d0c9bab3983ab7c6e85996c969bbc6ff596cc5
acdiaconu <40471624+acdiaconu@users.noreply.github.com>
2021-05-01 15:46:21 +0100

    Added limiter for invites (#292)
    A non-admin user can now invite at most 1 other user / week. This does
    not apply to admin users. If there is more than 1 try / week, an alert
    is shown, displaying the cooldown duration.

--- app/lib/exceptions.rb
+++ app/lib/exceptions.rb
@@ -9,7 +9,16 @@ module Mastodon
   class DimensionsValidationError < ValidationError; end
   class StreamValidationError < ValidationError; end
   class RaceConditionError < Error; end
-  class RateLimitExceededError < Error; end
+  
+  class RateLimitExceededError < StandardError
+    def initialize(cooldown_end, generated_via)
+      if generated_via = 'invite'
+        super("Sorry, you can only create 1 invite per week. Try again on #{cooldown_end}.")
+      else 
+        super("You have done this action too many times! Try  again on #{cooldown_end}.")
+      end
+    end
+  end
 
   class UnexpectedResponseError < Error
     def initialize(response = nil)
