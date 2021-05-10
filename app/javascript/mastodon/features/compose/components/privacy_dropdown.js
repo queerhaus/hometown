9ae228a20fce0917f674d638a3fdfcae0575e8bf
exstral <git@exstral.eu>
2021-05-10 22:01:52 +0200

    queerhaus: privacy dropdown hardcoded text

--- app/javascript/mastodon/features/compose/components/privacy_dropdown.js
+++ app/javascript/mastodon/features/compose/components/privacy_dropdown.js
@@ -234,8 +234,8 @@ class PrivacyDropdown extends React.PureComponent {
   componentWillMount () {
     const { intl: { formatMessage } } = this.props;
     // TODO get the real instance name and domain here
-    const instance = 'Local';
-    const domain = 'this community';
+    const instance = 'Queer Haus';
+    const domain = 'queer.haus';
 
     this.options = [
       { icon: 'users', value: 'local_only', text: formatMessage(messages.local_only_short, { instance }), meta: formatMessage(messages.local_only_long, { domain }) },
