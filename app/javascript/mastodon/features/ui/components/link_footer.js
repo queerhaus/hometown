3db6ff486f06f2507fccdf20cf6cb57be21ea8ca
kiridi <kiri.diaconu@gmail.com>
2021-05-14 17:15:08 +0100

    fix cropped footer

--- app/javascript/mastodon/features/ui/components/link_footer.js
+++ app/javascript/mastodon/features/ui/components/link_footer.js
@@ -45,33 +45,35 @@ class LinkFooter extends React.PureComponent {
     const { withHotkeys } = this.props;
 
     return (
-      <div className='getting-started__footer'>
-        <p>
-          Welcome to Queer Haus! Don't be shy, click around, explore, and please read <a href="/about/more">our about page that explains the basics</a>.
-        </p>
-        <p>
-          If something looks broken, send us a message.
-          You can reach us at our collective account <a href="/web/accounts/191">@dolphin</a> or our personal accounts:<br/>
-          <a href="/web/accounts/5">@exstral</a> <a href="/web/accounts/166">@vincentreynaud</a> <a href="/web/accounts/167">@RustySofa</a> <a href="/web/accounts/234">@monkysoda</a> <a href="https://queer.haus/web/accounts/302">@blablablorges</a> <a href="https://queer.haus/web/accounts/304">@marum</a>
-        </p>
-        <p>
-          Let’s communicate and organise! 🔥
-        </p>
+      <div className='getting-started__footer_container'>
+        <div className='getting-started__footer'>
+          <p>
+            Welcome to Queer Haus! Don't be shy, click around, explore, and please read <a href="/about/more">our about page that explains the basics</a>.
+          </p>
+          <p>
+            If something looks broken, send us a message.
+            You can reach us at our collective account <a href="/web/accounts/191">@dolphin</a> or our personal accounts:<br/>
+            <a href="/web/accounts/5">@exstral</a> <a href="/web/accounts/166">@vincentreynaud</a> <a href="/web/accounts/167">@RustySofa</a> <a href="/web/accounts/234">@monkysoda</a> <a href="https://queer.haus/web/accounts/302">@blablablorges</a> <a href="https://queer.haus/web/accounts/304">@marum</a>
+          </p>
+          <p>
+            Let’s communicate and organise! 🔥
+          </p>
 
-        <ul>
-          <li><a href='/about/more' target='_blank'>About us</a> · </li>
-          <li><a href='https://opencollective.com/queerhaus' target='_blank'><FormattedMessage id='getting_started.donate' defaultMessage='Donate' /></a> · </li>
-          <li><a href='https://matrix.to/#/#queerhaus:matrix.org?via=matrix.org' target='_blank'>Chat</a> · </li>
-          <li><a href='/about/more#apps-on-mobile' target='_blank'>Mobile&nbsp;apps</a> · </li>
-          <li><a href='/terms' target='_blank'><FormattedMessage id='getting_started.terms' defaultMessage='Terms of service' /></a> · </li>
-          <li><a href='https://docs.joinmastodon.org' target='_blank'><FormattedMessage id='getting_started.documentation' defaultMessage='Documentation' /></a> · </li>
-          <li><a href='/auth/sign_out' onClick={this.handleLogoutClick}><FormattedMessage id='navigation_bar.logout' defaultMessage='Logout' /></a></li>
-        </ul>
+          <ul>
+            <li><a href='/about/more' target='_blank'>About us</a> · </li>
+            <li><a href='https://opencollective.com/queerhaus' target='_blank'><FormattedMessage id='getting_started.donate' defaultMessage='Donate' /></a> · </li>
+            <li><a href='https://matrix.to/#/#queerhaus:matrix.org?via=matrix.org' target='_blank'>Chat</a> · </li>
+            <li><a href='/about/more#apps-on-mobile' target='_blank'>Mobile&nbsp;apps</a> · </li>
+            <li><a href='/terms' target='_blank'><FormattedMessage id='getting_started.terms' defaultMessage='Terms of service' /></a> · </li>
+            <li><a href='https://docs.joinmastodon.org' target='_blank'><FormattedMessage id='getting_started.documentation' defaultMessage='Documentation' /></a> · </li>
+            <li><a href='/auth/sign_out' onClick={this.handleLogoutClick}><FormattedMessage id='navigation_bar.logout' defaultMessage='Logout' /></a></li>
+          </ul>
 
-        <p>
-          <span>logo by Landon Whittaker</span><br/>
-          <span>all our projects are <a rel='noopener noreferrer' href='https://github.com/queerhaus'>open source</a></span>
-        </p>
+          <p>
+            <span>logo by Landon Whittaker</span><br/>
+            <span>all our projects are <a rel='noopener noreferrer' href='https://github.com/queerhaus'>open source</a></span>
+          </p>
+        </div>
       </div>
     );
   }
