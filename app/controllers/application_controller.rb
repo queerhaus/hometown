0c6b6f792162c341918cf0a16a6ee87dbd87b6f3
kiridi <kiri.diaconu@gmail.com>
2021-05-28 18:09:04 +0200

    Added limiter for invites (#292)
    A non-admin user can now invite at most 1 other user / week. This does
    not apply to admin users. If there is more than 1 try / week, an alert
    is shown, displaying the cooldown duration.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -105,8 +105,8 @@ class ApplicationController < ActionController::Base
     respond_with_error(503)
   end
 
-  def too_many_requests
-    respond_with_error(429)
+  def too_many_requests(exc)
+    respond_with_alert(exc)
   end
 
   def single_user_mode?
@@ -139,5 +139,10 @@ class ApplicationController < ActionController::Base
       format.any  { render "errors/#{code}", layout: 'error', status: code, formats: [:html] }
       format.json { render json: { error: Rack::Utils::HTTP_STATUS_CODES[code] }, status: code }
     end
+  end
+
+  def respond_with_alert(exc)
+    flash[:alert] = exc.message
+    redirect_back fallback_location: root_path
   end
 end
